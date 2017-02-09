5ef6243897476d2510f59586fd1a31af38558b30
ChrisBurgin <chrisburgin95@gmail.com>
2017-02-09 12:04:36 -0500

    Formatted Printing of items

--- src/js/components/SidebarItems/index.jsx
+++ src/js/components/SidebarItems/index.jsx
@@ -24,16 +24,19 @@ class SidebarItems extends React.Component {
       <div className="sidebaritems">
         {(
           Object.keys(this.props.items).map((key) =>
-            containsFilter(this.props.filter.searchText, this.props.items[key].tags, this.props.items[key].content)
-              ? <SidebarItem
-                store={this.store}
-                key={key}
-                refKey={key}
-                content={this.props.items[key].content}
-                date={this.props.items[key].date}
-                tags={this.props.items[key].tags}
-                active={isActive(this.props.activeItem, key)}/>
-              : false
+            containsFilter(
+              this.props.filter.searchText,
+              this.props.items[key].tags,
+              this.props.items[key].content)
+                ? <SidebarItem
+                  store={this.store}
+                  key={key}
+                  refKey={key}
+                  content={this.props.items[key].content}
+                  date={this.props.items[key].date}
+                  tags={this.props.items[key].tags}
+                  active={isActive(this.props.activeItem, key)}/>
+                : false
           )
         )}
       </div>
